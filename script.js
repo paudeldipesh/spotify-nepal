affde37bc746384e244d6a2874cb30d6f2d5a08d
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2024-01-28 21:47:02 +0545

    setting up for a song and a song card

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Script is working");
+async function getSongs() {
+  const response = await fetch("http://127.0.0.1:3000/songs/");
+  const data = await response.text();
+
+  let div = document.createElement("div");
+  div.innerHTML = data;
+
+  const anchor = div.getElementsByTagName("a");
+  let songs = [];
+
+  for (let index = 0; index < anchor.length; index++) {
+    const element = anchor[index];
+
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
+    }
+  }
+  console.log(songs);
+  return songs;
+}
+
+async function main() {
+  let songs = await getSongs();
+
+  let songsUl = document
+    .querySelector(".song-list")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songsUl.innerHTML += `<li>${
+      song.replaceAll("%20", " ").split(".mp3")[0]
+    }</li>`;
+  }
+
+  let audio = new Audio(songs[0]);
+  audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+  });
+}
+main();
