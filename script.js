195a8296ed23f75f3f0d7786c370c49875c9b03a
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2024-01-29 13:18:27 +0545

    working on playbar and seekbar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const playBtnEl = document.querySelector(".play");
+playBtnEl.addEventListener("click", () => {
+  console.log("Music played");
+});
+
 async function getSongs() {
   const response = await fetch("http://127.0.0.1:3000/songs/");
   const data = await response.text();
@@ -15,20 +20,32 @@ async function getSongs() {
       songs.push(element.href.split("/songs/")[1]);
     }
   }
-  console.log(songs);
   return songs;
 }
 
-async function main() {
+(async function main() {
   let songs = await getSongs();
 
   let songsUl = document
     .querySelector(".song-list")
     .getElementsByTagName("ul")[0];
+
   for (const song of songs) {
-    songsUl.innerHTML += `<li>${
-      song.replaceAll("%20", " ").split(".mp3")[0]
-    }</li>`;
+    songsUl.innerHTML += `<li class="song-card rounded">
+    <img class="invert" src="music.svg" alt="music" />
+    <div class="info">
+      <div class="text-orange w-20">
+        ${song.replaceAll("%20", " ").split(".mp3")[0].split("-")[1]}
+      </div>
+      <div class="text-green text-sm">
+        ${song.replaceAll("%20", " ").split(".mp3")[0].split("-")[0]}
+      </div>
+    </div>
+    <span class="cursor-pointer play-now">Play Now</span>
+    <div class="play-button cursor-pointer">
+      <img class="invert cursor-pointer" src="play.svg" alt="play" />
+    </div>
+  </li>`;
   }
 
   let audio = new Audio(songs[0]);
@@ -37,5 +54,4 @@ async function main() {
   audio.addEventListener("loadeddata", () => {
     console.log(audio.duration, audio.currentSrc, audio.currentTime);
   });
-}
-main();
+})();
